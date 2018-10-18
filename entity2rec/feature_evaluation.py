fedaaece31cc60099e9c597acf0c0c7085dcb26a
enricopal <enricopalumbo0@gmail.com>
2018-10-18 17:48:41 +0200

    feature evaluation without training

--- entity2rec/feature_evaluation.py
+++ entity2rec/feature_evaluation.py
@@ -3,14 +3,17 @@ from evaluator import Evaluator
 import time
 from parse_args import parse_args
 import numpy as np
+import random
 
-np.random.seed(1)  # fixed seed for reproducibility
+random.seed(1)  # fixed seed for reproducibility
 
 start_time = time.time()
 
 print('Starting entity2rec...')
 
 args = parse_args()
+
+# default settings
 
 if not args.train:
     args.train = 'datasets/'+args.dataset+'/train.dat'
@@ -21,124 +24,49 @@ if not args.test:
 if not args.validation:
     args.validation = 'datasets/'+args.dataset+'/val.dat'
 
+if args.dataset == 'LastFM':
+
+    implicit = True
+
+else:
+
+    implicit = args.implicit
+
+if args.dataset == 'LibraryThing':
+
+    threshold = 8
+
+else:
+
+    threshold = args.threshold
+
+
 # initialize entity2rec recommender
 e2rec = Entity2Rec(args.dataset, run_all=args.run_all, p=args.p, q=args.q,
-                   feedback_file=args.feedback_file, walk_length=args.walk_length,
+                   walk_length=args.walk_length,
                    num_walks=args.num_walks, dimensions=args.dimensions, window_size=args.window_size,
-                   workers=args.workers, iterations=args.iter, collab_only=args.collab_only,
-                   content_only=args.content_only)
+                   workers=args.workers, iterations=args.iter)
 
 # initialize evaluator
 
-evaluat = Evaluator(implicit=args.implicit, threshold=args.threshold, all_unrated_items=args.all_unrated_items)
+evaluat = Evaluator(implicit=implicit, threshold=args.threshold, all_unrated_items=args.all_unrated_items)
 
 # compute e2rec features
 x_train, y_train, qids_train, items_train, x_test, y_test, qids_test, items_test,\
     x_val, y_val, qids_val, items_val = evaluat.features(e2rec, args.train, args.test,
-                                          validation=args.validation, n_users=args.num_users, n_jobs=args.workers)
+                                          validation=False, supervised=False,
+                                          n_users=args.num_users, n_jobs=args.workers)
 
 print('Finished computing features after %s seconds' % (time.time() - start_time))
 
-if args.write_features:
-    print('writing features to file...')
-    evaluat.write_features_to_file('train', qids_train, x_train, y_train, items_train)
+for i, prop in enumerate(e2rec.properties):
 
-    evaluat.write_features_to_file('test', qids_test, x_test, y_test, items_test)
+    print(prop.name)
 
-    if args.validation:
+    test_feat = x_test[:, i]
+    test_feat = test_feat.reshape((-1, 1))
 
-        evaluat.write_features_to_file('val', qids_val, x_val, y_val, items_val)
-
-print('Starting to fit the model...')
-
-with open('feature_evaluation_%s_p%d_q%d.csv' %(args.dataset, args.p, args.q), 'w') as feature_eval_file:
-
-    feature_eval_file.write('feature,P@5,P@10,MAP,R@5,R@10,NDCG,MRR\n')  # write header
-
-    feature_eval_file.write('all,')
-
-    e2rec.fit(x_train, y_train, qids_train,
-              x_val=x_val, y_val=y_val, qids_val=qids_val, optimize=args.metric, N=args.N)  # train the model
-
-    scores = evaluat.evaluate(e2rec, x_test, y_test, qids_test, items_test,
-                              verbose=False)  # evaluates the recommender on the test set
-
-    for _, score in scores[0:-1]:
-
-        feature_eval_file.write('%f,' % score)
-
-    feature_eval_file.write('%f\n' % scores[-1][1])
-
-    feature_eval_file.flush()
-
-    for i, prop in enumerate(e2rec.properties):
-
-        print(prop.name)
-
-        feature_eval_file.write('%s,' % prop.name)
-
-        # fit e2rec on one feature at the time
-
-        train_feat = x_train[:, i]
-        train_feat = train_feat.reshape((-1, 1))
-
-        val_feat = x_val[:, i]
-        val_feat = val_feat.reshape((-1, 1))
-
-        test_feat = x_test[:, i]
-        test_feat = test_feat.reshape((-1, 1))
-
-        e2rec.fit(train_feat, y_train, qids_train,
-                  x_val=val_feat, y_val=y_val, qids_val=qids_val, optimize=args.metric, N=args.N)  # train the model
-
-        scores = evaluat.evaluate(e2rec, test_feat, y_test, qids_test, items_test,
-                                  verbose=False)  # evaluates the recommender on the test set
-
-        for _, score in scores[0:-1]:
-
-            feature_eval_file.write('%f,' % score)
-
-        feature_eval_file.write('%f\n' % scores[-1][1])
-
-        feature_eval_file.flush()
-
-    """
-    #no_social_properties_indexes = []
-
-    #for i, prop in enumerate(e2rec.properties):
-
-    #    if prop.typology == "social":
-    #        continue
-
-    #    else:
-    #        no_social_properties_indexes.append(i)
-
-    #print("no social properties")
-
-    #feature_eval_file.write('no_social,')
-
-    # fit e2rec on one feature at the time
-    
-    train_feat = x_train[:, no_social_properties_indexes]
-    train_feat = train_feat.reshape((-1, len(no_social_properties_indexes)))
-
-    val_feat = x_val[:, no_social_properties_indexes]
-    val_feat = val_feat.reshape((-1, len(no_social_properties_indexes)))
-
-    test_feat = x_test[:, no_social_properties_indexes]
-    test_feat = test_feat.reshape((-1, len(no_social_properties_indexes)))
-
-    e2rec.fit(train_feat, y_train, qids_train,
-              x_val=val_feat, y_val=y_val, qids_val=qids_val, optimize=args.metric, N=args.N)  # train the model
-
-    scores = evaluat.evaluate(e2rec, test_feat, y_test, qids_test,
-                              verbose=False)  # evaluates the recommender on the test set
-
-    for _, score in scores[0:-1]:
-        feature_eval_file.write('%f,' % score)
-
-    feature_eval_file.write('%f\n' % scores[-1][1])
-
-    feature_eval_file.flush()
-    """
+    scores = evaluat.evaluate(e2rec, test_feat, y_test, qids_test, items_test,
+                              verbose=False, write_to_file='results/%s/%s.csv' % (args.dataset,
+                              prop.name))  # evaluates the recommender on the test set
 
